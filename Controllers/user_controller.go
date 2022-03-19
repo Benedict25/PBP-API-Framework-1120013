d697f445cdc912575cda70f2d42444b0e7b7a2ce
Benedict <benedictivan25@yahoo.com>
2022-03-19 12:03:30 +0700

    Test Get Endpoint

--- Controllers/user_controller.go
+++ Controllers/user_controller.go
@@ -1,7 +1,37 @@
 package Controllers
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func test() {
-	fmt.Print("test")
+	"github.com/gin-gonic/gin"
+)
+
+func GetProducts(c *gin.Context) {
+
+	db := Connect()
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+
+	var user User
+	var users []User
+
+	for results.Next() {
+		err = results.Scan(&user.ID, &user.Name, &user.Address)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+
+	if len(users) == 0 {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.IndentedJSON(http.StatusOK, users)
+	}
+
 }
